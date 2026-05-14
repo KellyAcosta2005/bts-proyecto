5fbdbbe4812aba4483e09b9541c21bd79c14394f
KellyAcosta2005 <knacosta15@gmail.com>
2026-05-13 21:29:34 -0300

    cambios al css y al index
     agregar agenda de gira, carrusel y navegacion fija, Implementacion de secciones de tour y galería con carrusel interactivo. Se ajustó el CSS para navegación fija y scroll suave

--- js/script.js
+++ js/script.js
@@ -9,3 +9,33 @@ document.getElementById('btnEnviar').addEventListener('click', function() {
         alert("Por favor, ingresa tu nombre.");
     }
 });
+
+const track = document.getElementById('track');
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+const dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+}
+
+nextBtn.addEventListener('click', () => {
+    index = (index + 1) % 5;
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    index = (index - 1 + 5) % 5;
+    updateCarousel();
+});
+
+dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+        index = i;
+        updateCarousel();
+    });
+});
